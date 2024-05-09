d927905d786a67a09ede28fa7e858cf1682a6eaa
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-05-09 18:29:10 -0500

    Added Authorization Files in Client and Server Directories
     - Server Side:
        - Added authorization middleware and token functionality in authMiddleware.js
        - Created dotenv file with JWT secret key
        - Installed dotenv as a dependency
     - Client Side:
        - Added AuthService class for client side authorization functions
        - Installed jwt-decode as a dependency

---
+++ client/src/utils/auth.js
@@ -0,0 +1,82 @@
+import { jwtDecode } from 'jwt-decode';
+
+
+// create a new class to instantiate for a user
+class AuthService {
+  // get user data
+  getProfile() {
+    const token = this.getToken();
+    if (token) {
+      return jwtDecode(token);
+    }
+    return null;
+  }
+
+
+  // check if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
+
+  // check if token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = jwtDecode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true; // Token is expired
+      } else {
+        return false; // Token is not expired
+      }
+    } catch (err) {
+      return false; // Unable to decode token, consider it not expired
+    }
+  }
+  
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('token');
+  }
+
+  login(token) {
+    // Saves user token to localStorage
+    console.log('auth.js:', token)
+    localStorage.setItem('token', token);
+    // window.location.assign('/');
+  }  
+
+  isAuthenticated() {
+    // Delegate to the loggedIn method
+    return this.loggedIn();
+  }
+
+  getUserId() {
+    const token = this.getToken();
+    console.log('AUTH.JS: TOKEN', token)
+    try {
+      console.log('GETUSERID TRY INITIATED');
+      if (token) {
+        const decoded = jwtDecode(token);
+        console.log('If Statement decoded:', decoded);
+        console.log('LOGGED IN USER ID IN AUTH:', decoded.data._id);
+
+        return decoded.data._id;
+      }
+    } catch (error) {
+      console.error('GetUserID ERROR', error);
+      return null;
+    }
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
+
+const authServiceInstance = new AuthService();
+export default authServiceInstance;
---
+++ server/utils/authMiddleware.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); 
+
+const secret = process.env.JWT_SECRET_KEY || 'default_secret';
+
+function authMiddleware(req, res, next) {
+
+    const nonAuthRoutes = ['/', '/login', '/signup'];
+
+    if (nonAuthRoutes.includes(req.path)) {
+        return next();
+    }
+
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization && token.startsWith('Bearer ')) {
+        token = token.slice(7);
+    }
+
+
+    console.log('authMiddleware Token', token);
+
+    if (!token) {
+        return next();
+    }
+
+    try {
+        const decoded = jwt.verify(token, secret);
+        req.user = decoded.data;
+        next();
+    } catch (error) {
+        console.error('Error in authentication:', error);
+        // req.user = null;
+        res.status(401).json({ message: 'Invalid token' });
+    }
+}
+
+function signToken(user) {
+    const { email, _id } = user;
+    console.log('Signing token for user:', email, _id);
+
+    const payload = { email, _id };
+    const token = jwt.sign({ data: payload }, secret);
+
+    console.log('Generated token:', token);
+
+    return token;
+}
+
+module.exports = { authMiddleware, signToken };
